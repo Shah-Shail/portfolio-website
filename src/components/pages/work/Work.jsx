a1911daa1d7216e4ab733dc6f9a24cfe4c7da935
Shail <shailshah.officework@gmail.com>
2024-10-05 22:58:11 +0530

    completed work module ui

--- src/components/pages/work/Work.jsx
+++ src/components/pages/work/Work.jsx
@@ -4,46 +4,12 @@ import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 
-import { BsArrowUpRight, BsGithub } from 'react-icons/bs'
+// import { BsArrowUpRight, BsGithub } from 'react-icons/bs'
 
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../../ui/tooltip'
+// import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../../ui/tooltip'
 import WorkSliderBtns from '../../common/work-slider-btn'
+import { projects } from './data'
 
-const projects = [
-  {
-    num: '01',
-    category: 'frontend',
-    title: 'project 1',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsum numquam aliquid.',
-    stack: [{ name: 'Html 5' }, { name: 'Css 3' }, { name: 'Javascript' }],
-    image: '/assets/work/thumb1.png',
-    live: '',
-    github: ''
-  },
-  {
-    num: '02',
-    category: 'fullstack',
-    title: 'project 2',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsum numquam aliquid.',
-    stack: [{ name: 'Next.js' }, { name: 'Tailwind.css' }, { name: 'Node.js' }],
-    image: '/assets/work/thumb2.png',
-    live: '',
-    github: ''
-  },
-  {
-    num: '03',
-    category: 'frontend',
-    title: 'project 3',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsum numquam aliquid.',
-    stack: [{ name: 'React.js' }, { name: 'Tailwind.css' }],
-    image: '/assets/work/thumb3.png',
-    live: '',
-    github: ''
-  }
-]
 const Work = () => {
   const [project, setProject] = useState(projects[0])
 
@@ -61,21 +27,35 @@ const Work = () => {
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            <div className="flex flex-col gap-[30px] h-[50%]">
+        <div className="flex flex-col lg:flex-row lg:gap-[30px]">
+          <div className="w-full lg:w-[50%] flex flex-col xl:justify-between order-2 lg:order-none">
+            <div className="flex flex-col gap-[20px] h-[50%]">
               {/* outline num */}
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project.num}
               </div>
-              {/* project category */}
-              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
-                {project.category} project
-              </h2>
+              <div>
+                {/* project category */}
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                  {project.name}
+                </h2>
+                {/* project type */}
+                <p className="w-fit bg-pink-50/20 text-white text-sm font-medium mt-2 px-2.5 py-0.5 rounded">
+                  {project.projectType}
+                </p>
+              </div>
               {/* project description */}
-              <p className="text-white/60">{project.description}</p>
+              <ul className="flex flex-col gap-[10px] list-disc marker:text-accent px-6">
+                {project.description.map((item, index) => {
+                  return (
+                    <li key={index} className="text-[16px] text-white/60">
+                      {item.text}
+                    </li>
+                  )
+                })}
+              </ul>
               {/* stack */}
-              <ul className="flex gap-4">
+              <ul className="flex flex-wrap gap-4">
                 {project.stack.map((item, index) => {
                   return (
                     <li key={index} className="text-xl text-accent">
@@ -87,10 +67,11 @@ const Work = () => {
                 })}
               </ul>
               {/* border */}
-              <div className="border border-white/20"></div>
+              {/*
+                <div className="border border-white/20"></div>
+                */}
               {/* buttons */}
-              <div className="flex items-center gap-4">
-                {/* live project button */}
+              {/* <div className="flex items-center gap-4">
                 <TooltipProvider delayDuration={100}>
                   <Tooltip>
                     <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
@@ -101,7 +82,6 @@ const Work = () => {
                     </TooltipContent>
                   </Tooltip>
                 </TooltipProvider>
-                {/* github project button */}
                 <TooltipProvider delayDuration={100}>
                   <Tooltip>
                     <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
@@ -112,37 +92,51 @@ const Work = () => {
                     </TooltipContent>
                   </Tooltip>
                 </TooltipProvider>
-              </div>
+              </div> */}
             </div>
           </div>
-          <div className="w-full xl:w-[50%]">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity: 1,
+              transition: { delay: 1.8, duration: 0.4, ease: 'easeIn' }
+            }}
+            className="w-full lg:w-[50%]"
+          >
             <Swiper
               spaceBetween={30}
               slidesPerView={1}
-              className="xl:h-[520px] mb-12"
+              className="lg:h-[600px] mb-12"
               onSlideChange={handleSlideChange}
             >
               {projects.map((project, index) => {
                 return (
                   <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    <motion.div
+                      initial={{ opacity: 0 }}
+                      animate={{
+                        opacity: 1,
+                        transition: { delay: 1, duration: 0.4, ease: 'easeInOut' }
+                      }}
+                      className="h-[460px] relative group flex justify-center items-center bg-pink-50/20"
+                    >
                       {/* overlay */}
-                      <div className="absoulute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                       {/* image */}
                       <div className="relative w-full h-full">
-                        <img src={project.image} className="object-cover" alt="" />
+                        <img src={project.image} className="w-full h-full" alt="" />
                       </div>
-                    </div>
+                    </motion.div>
                   </SwiperSlide>
                 )
               })}
               {/* Slider buttons*/}
               <WorkSliderBtns
-                containerSytyles="flex gap-2 absolute right-0 button-[calc(50% - 22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                containerSytyles="flex gap-2 absolute right-0 button-[calc(50% - 22px)] lg:bottom-0 z-20 w-full justify-between lg:w-max lg:justify-none"
                 btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22] w-[44px] h-[44px] flex justify-center items-center transiltion-all"
               />
             </Swiper>
-          </div>
+          </motion.div>
         </div>
       </div>
     </motion.section>
---
+++ src/components/pages/work/data.js
@@ -0,0 +1,66 @@
+export const projects = [
+  {
+    num: '01',
+    name: 'PROACT',
+    projectType: 'Company',
+    title: 'project 1',
+    description: [
+      {
+        text: 'In this healthcare project, I have experience with various packages including react-table, react-hook-form, react-select, react-zoom-pan-pinch, and several others.'
+      },
+      {
+        text: 'Developed a Report Builder module allowing users to generate dynamic reports based on their needs.'
+      },
+      { text: ' Also developed modules, incorporated essential packages, and integrated APIs.' }
+    ],
+    stack: [{ name: 'React.js' }, { name: 'Javascript' }, { name: 'Redux' }, { name: 'SCSS' }],
+    image: '/assets/work/thumb1.png',
+    live: '',
+    github: ''
+  },
+  {
+    num: '02',
+    name: 'OPSACUITY',
+    projectType: 'Company',
+    title: 'project 2',
+    description: [
+      {
+        text: 'Contributed to the ERP system by setting up the folder structure and configuring the routing architecture for different modules.'
+      },
+      {
+        text: 'I implemented comprehensive Roles and Permissions management and also utilized the ApexCharts package for graphs.'
+      },
+      {
+        text: 'I implemented animations in several parts of the module to make it more interactive and visually appealing for users.'
+      }
+    ],
+    stack: [{ name: 'React.js' }, { name: 'TypeScript' }, { name: 'Redux' }, { name: 'CSS' }],
+    image: '/assets/work/thumb2.png',
+    live: '',
+    github: ''
+  },
+  {
+    num: '03',
+    name: 'SYNAPP',
+    projectType: 'Company',
+    title: 'project 3',
+    description: [
+      {
+        text: 'In this healthcare project, I developed responsive modules and layouts with UI libraries like Ant Design, integrated essential packages, and connected APIs.'
+      },
+      {
+        text: 'I developed utility functions and custom hooks for common use throughout the project.'
+      }
+    ],
+    stack: [
+      { name: 'React.js' },
+      { name: 'Javascript' },
+      { name: 'Redux' },
+      { name: 'Ant Design' },
+      { name: 'CSS' }
+    ],
+    image: '/assets/work/thumb3.png',
+    live: '',
+    github: ''
+  }
+]
